08d10fda0585447bacdbf829215b3b97408bb8b0
Juan Oyarzun Gajardo <juanoyarzungajardo@MacBook-Pro-de-Juan-2.local>
2021-06-30 18:39:23 -0400

    Add custom hooks in BadgesList

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -4,7 +4,16 @@ import Badge from '../components/Badge';
 import { Link } from 'react-router-dom';
 import DeleteBadgeModal from '../components/DeleteBadgeModal';
 
+function useIncreaseCount(max) {
+    const [count, setCount] = React.useState(0);
+    if (count > max) {
+        setCount(0);
+    }
+    return [count, setCount];
+}
+
 function BadgeDetails(props) {
+    const [count, setCount] = useIncreaseCount(4);
     const badge = props.badge;
 
     return (
@@ -29,6 +38,9 @@ function BadgeDetails(props) {
                     <div className="col">
                         <h2>Actions</h2>
                         <div>
+                            <button onClick={() => {
+                                setCount(count + 1)
+                            }} className="btn btn-primary mr-4">Increase Count {count}</button>
                             <Link className="btn btn-primary mb-4" to={`/badges/${badge.id}/edit`}>Edit</Link>
                         </div>
                         <div>
